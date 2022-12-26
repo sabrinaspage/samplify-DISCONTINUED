3b6fb1edd4f77a10cbca7a09596d92103e897350
Sabrina <54827482+sabrinaspage@users.noreply.github.com>
2022-12-26 14:08:13 -0500

    discontinuing
    amplify is just a bunch of sdks that simplifies the interaction between your app and aws services. probably wouldnt use beyond tutorial.

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -5,5 +5,16 @@ export type AmplifyDependentResourcesAttributes = {
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
         }
+    },
+    "auth": {
+        "sabsportfolioed1a65ac": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
     }
 }
